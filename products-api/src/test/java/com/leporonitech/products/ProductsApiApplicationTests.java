14969728a756fe69581f4cef60d69f5ede0ca2ed
Leporoni <leporoni@hotmail.com>
2023-12-27 11:42:26 -0300

    Uploading other files like docker, maven and application.yml etc...

---
+++ products-api/src/test/java/com/leporonitech/products/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leporonitech.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
