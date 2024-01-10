e97e4a65bdd6394168c5989fef85f07da975fbb3
Leporoni <leporoni@hotmail.com>
2024-01-10 11:05:05 -0300

    several adjustments

--- payments-api/src/main/java/com/leporonitech/payments/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leporonitech.payments.repository;
-
-import com.leporonitech.payments.model.Payment;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends CrudRepository<Payment, Long> {
-}
